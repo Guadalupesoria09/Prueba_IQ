d372c7ccfd29a4a25a620ed05f3c63dd244f6dee
Guadalupesoria09 <111298699+Guadalupesoria09@users.noreply.github.com>
2022-08-25 21:21:04 -0500

    Create Algoritmo+avance2.py

---
+++ Algoritmo+avance2.py
@@ -0,0 +1,105 @@
+Algoritmo_suma
+
+    suma= float (input{"1.¿Cuál es la suma de 10^2+108%10+754?"})
+      if resul= return sum
+           x= 10
+           y= 108
+           z= 754
+          def suma (x,y,z)
+          sum=({x**2})+({y%x})+(z)
+          return sum 
+          print("la respuesta es correcta")
+          def califica1
+          if resul= (return sum)  
+          Entonces calificaciom +1 
+      SiNo 
+          print("respuesta incorrecta")
+          califica 0
+
+
+
+      Floor= int (input{"2.¿Cuál es el numero entero más cercano de la divición 30/7"})
+      if result= return Floor
+         x=30
+         y=7
+         def floor (x//y)
+         Floor=({30//7})
+         return Floor
+         print ("La respuesta es correcta")
+         def califica2
+         if result= (return Floor)
+         Entonces calificacion +1
+       SiNo
+          print("respuesta incorrecta")
+          califica 0
+
+
+       Substration= float (input{"3.Calcula la resta de 7658-12^3"})
+       if result= return Substration
+          x=7658
+          y=12^2
+          def Substration(x-y**3)
+          Substration= ({7658})-({12**2})
+          return Substration
+          print ("La respuesta es correcta")
+          def califica3
+          if result= (return Substration)
+           Entonces calificacion +1
+       SiNo
+          print("respuesta incorrecta")
+          califica 0
+
+        
+        Mult= int (input {"4.Calcula la siguiente multiplicación: 15^2*5*2^2"})
+        if result= return Mult
+           x=15^2
+           y=5
+           z=2^2
+           def Mult (x**2-y-z**2)
+           Mult= ({15**2})-({5})-({2**2})
+           return Mult
+           print ("La respuesta es correcta")
+          def califica4
+          if result= (return Mult)
+           Entonces calificacion +1
+       SiNo
+          print("respuesta incorrecta") 
+          califica 0
+
+         
+         Modulos= float (input{"5. Calcula el resultado de 20%2+5%1-9/8*4"})
+         if result= return Modulos
+           x=20
+           y=2
+           z=5
+           h=1
+           j=9
+           k=8
+           l=4
+           def Modulos (x%y+z%h-j/k*l)
+           Modulos= ({20%2})+({5%1})-({9/8})*({4})
+           return Modulos
+           print ("La respuesta es correcta")
+           def califica5
+           if result= (return Modulos)
+           Entonces calificacion +1
+       SiNo
+          print("respuesta incorrecta") 
+          califica 0
+
+          def califinal (califica1,califica2,califica3,califica4,califica5)
+          final= ({1})+({1})+({1})+({1})+({1})
+          return final 
+          print (f"calificación final {califinal(califica1,califica2,califica3,califica4,califica5)}")
+
+          def IQ 
+          if Calificacion_final= (>=4)
+             entonces IQ= 160
+             or Calificacion_final= (2<4)
+                entonces IQ= 140
+             or Calificacion_final= (<2)
+                entonces Iq= 120
+                return Calificacion_final
+          print (f"IQ {return Calificacion_final}")
+
+FinAlgoritmo
