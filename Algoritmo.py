d33a8b290294de4e1ac745282a616a8c717f1e12
Guadalupesoria09 <111298699+Guadalupesoria09@users.noreply.github.com>
2022-09-29 19:53:45 -0500

    Update Algoritmo.py

--- Algoritmo.py
+++ Algoritmo.py
@@ -12,101 +12,30 @@ if op==salir:
 else:
     print ("comencemos")
 
-#CALIFICACIÓN 
-def calificacion (respuesta1,respuesta2,respuesta3,respuesta4,respuesta5):
+def preguntar():
+    calif=0
+    indice=0
+    while indice<5:
+        print(preguntas[indice])
+        resp=input()
+        if resp==respuestas[indice]:
+            print("correcto")
+            calif+=1
+        indice=indice+1
+    return calif
 
-  #primera pregunta 
-  intentos = 0
-  correcto= 0
- while (intentos < 2):
-     print("1.¿Cuál es la suma de 10^2+108%10+754?")
-     respuesta= int (862)
-     respuesta1= int (input())
-      if respuesta1 == respuesta:
-          print ("Correcto")
-          correcto = correcto + 1 
-      else:
-          print ("Incorrecto") 
-          intentos = intentos + 1
-   if intentos == 2 :
-       print("Has ocupado todos tus intentos")
+preguntas = [
+    "1.¿Cuál es la suma de 10^2+108%10+754?",
+    "2.¿Cuál es el numero entero mas cercano de la división 30/7?",
+    "3.Calcula la resta de 7658-12^3",
+    "4.Calcula la siguiente multiplicación: 15^2*5*2^2",
+    "5.Calcular el resultado de 20%2+5%1-9/8*4"
+    ]
 
-      print ("oprime enter para continuar: ")
-      input ()
-   
-      #Suegunda pregunta
-     print("2.¿Cuál es el numero entero mas cercano de la división 30/7?")
-     respuesta02 = int (4)
-     respuesta2= int (input())
-      if respuesta2==respuesta02:
-         print ("Correcto")
-         correcto = correcto + 1 
-      else:
-         print("Incorrecto")
-         intentos = intentos + 1
-   if intentos == 2 :
-       print("Has ocupado todos tus intentos")
-    
-       print ("oprime enter para continuar: ")
-       input ()
+respuestas = ["862","4","5930","4500","-4.5"]
 
-       #Tercera pregunta 
-     print("3.Calcula la resta de 7658-12^3")
-     respuesta03= int (5930)
-     respuesta3= int (input())
-      if respuesta3== respuesta03:
-         print ("Correcto")
-         correcto = correcto + 1 
-      else:
-         print ("Incorrecto")
-         intentos = intentos + 1
-   if intentos = 2 :
-       print ("Has ocupado todos tus intentos")
-
-       print ("oprime enter para continuar: ")
-       input ()
-
-       #Cuarta pregunta 
-     print ("4.Calcula la siguiente multiplicación: 15^2*5*2^2")
-     respuesta04= int (4500)
-     respuesta4= int (input())
-      if respuesta4== respuesta04:
-         print("Correcto")
-         correcto = correcto + 1 
-      else:
-         print ("Incorrecto")
-         intentos = intentos + 1
-   if intentos == 2 :
-       print ("Has ocupado todos tus intentos")
-
-       print ("oprime enter para continuar: ")
-       input ()
-
-       #Quinta pregunta
-     print ("Calcular el resultado de 20%2+5%1-9/8*4")
-     respuesta05= float (-4.5)
-     respuesta5= float (input())
-      if respuesta5 == respuesta05:
-         print("Correcto")
-         correcto = correcto + 1 
-      else:
-         print("Incorecto")
-         intentos = intentos + 1
-   if intentos == 2 :
-       print ("Has ocupado todos tus intentos")
-    return aciertos
-
- resultado = calificacion (respuesta1,respuesta2,respuesta3,respuesta4,respuesta5)
- resultado = respuesta1 + respuesta2 + respuesta3 + respuesta4 + respuesta5
- return resultado 
-print (f"Su total de aciertos son: {resultado}") 
-if calificacion >0 and <2 :
-    print ("Su puntaje IQ es de 120")
-elif calificacion <=4 :
-    print ("Su puntaje de IQ es de 140")
-else:
-    print ("Su puntaje de IQ es de 160")
-
+calificacion=preguntar()
+print(f"Tu calificacion fue {calificacion/5}")
 
 
 
